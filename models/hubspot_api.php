f211c3144c98c4e736b39215b3a3e6bfb8b28bfa
MyStream <dev@mystream.co.uk>
2025-05-18 07:22:15 +0100

    Update hubspot_api.php

--- models/hubspot_api.php
+++ models/hubspot_api.php
@@ -12,30 +12,46 @@ class hubspot_service {
 	public static function ownerLoad(string $id): ?array;
 
 	// Contact Information
-	public static function contactLoad(string $email): ?array;
-	public static function contactSave(array $data): ?array;
-	public static function contactDelete(string $email): bool;
-	public static function contactLoad(string $id): ?array;
+	public static function contactLoadById(string $id): ?array;
+	public static function contactLoadByEmail(string $email): ?array;
 	public static function contactSearch(array $filters = [], int $limit = 20, int $offset = 0): array;
+	public static function contactUpdate(array $data): ?array;
+	public static function contactDelete(string $email): bool;
 
 	// Company Information
-	public static function companyLoad(string $domain): ?array;
-	public static function companySave(array $data): ?array;
-	public static function companyDelete(string $domain): bool;
-	public static function companyLoad(string $id): ?array;
+	public static function companyLoadById(string $id): ?array;
+	public static function companyLoadByName(string $domain): ?array;
 	public static function companySearch(array $filters = [], int $limit = 20, int $offset = 0): array;
+	public static function companyUpdate(array $data): ?array;
+	public static function companyDelete(string $domain): bool;
 
 	// Deal Information
-	public static function dealLoad(string $dealName): ?array;
-	public static function dealSave(array $data): ?array;
-	public static function dealDelete(string $dealId): bool;
-	public static function dealLoad(string $id): ?array;
+	public static function dealLoadById(string $id): ?array;
+	public static function dealLoadByName(string $dealName): ?array;
 	public static function dealSearch(array $filters = [], int $limit = 20, int $offset = 0): array;
+	public static function dealUpdate(array $data): ?array;
+	public static function dealDelete(string $dealId): bool;
 
+	// Deal Pipeline
+	public static function dealPipelineSearch(): array;
+	public static function dealPipelineLoad(string $pipelineId): array;
+	public static function dealPipelineCreate(array $data): array;
+	public static function dealPipelineUpdate(string $pipelineId, array $data): array;
+	public static function dealPipelineDelete(string $pipelineId): array;
+
+	// Deal Stage
+	public static function dealPipelineStageSearch(string $pipelineId): array;
+	public static function dealPipelineStageLoad(string $pipelineId, string $stageId): array;
+	public static function dealPipelineStageCreate(string $pipelineId, array $data): array;
+	public static function dealPipelineStageUpdate(string $pipelineId, string $stageId, array $data): array;
+	public static function dealPipelineStageDelete(string $pipelineId, string $stageId): array;
+
+	
 	// Linking Companies, Contacts and Deals
 	public static function associateContactWithCompany(string $contactId, string $companyId): bool;
 	public static function associateContactWithDeal(string $contactId, string $dealId): bool;
 	public static function associateDealWithCompany(string $dealId, string $companyId): bool;
+	public static function associateDealWithContact(string $dealId, string $contactId): array;
 
 	// Engagements
 	public static function engagementCreate(array $data): ?array;
@@ -51,20 +67,20 @@ class hubspot_service {
 	public static function productSearch(array $filters = [], int $limit = 20, int $offset = 0): array;
 	public static function productCreate(array $productData): array;
 	public static function productLoad(string $productId): array;
-	public static function productSave(array $data): ?array;
+	public static function productUpdate(array $data): ?array;
 	public static function productDelete(string $id): bool;
 
 	// Product Properties (schema)
 	public static function productPropertiesLoad(): array;
 	public static function productPropertyCreate(array $propertyData): array;
-	public static function productPropertySave(string $propertyName, array $propertyData): array;
+	public static function productPropertyUpdate(string $propertyName, array $propertyData): array;
 	public static function productPropertyDelete(string $propertyName): array;
 
 	// Product Folders
 	public static function productFoldersLoad(): array;
 	public static function productFolderCreate(array $folderData): array;
 	public static function productFolderLoad(string $folderId): array;
-	public static function productFolderSave(string $folderId, array $folderData): array;
+	public static function productFolderUpdate(string $folderId, array $folderData): array;
 	public static function productFolderDelete(string $folderId): array;
 
 	// Form Definitions
@@ -72,7 +88,7 @@ class hubspot_service {
 	public static function formSubmit(string $portalId, string $formGuid, array $fields, array $context = []): bool;
 	public static function formLoad(string $formId): array;
 	public static function formCreate(array $formData): array;
-	public static function formSave(string $formId, array $formData): array;
+	public static function formUpdate(string $formId, array $formData): array;
 	public static function formDelete(string $formId): array;
 	public static function formSubmissionsLoad(string $formId, array $filters = []): array;
 
@@ -89,7 +105,7 @@ class hubspot_service {
 	public static function fileDelete(string $fileId): array;
 	public static function fileLoad(string $fileId): array;
 	public static function fileList(array $filters = []): array;
-	public static function fileSave(string $fileId, array $updateData): array;
+	public static function fileUpdate(string $fileId, array $updateData): array;
 	public static function fileArchive(string $fileId): array;
 	public static function fileReplace(string $fileId, string $newFilePath, string $newFileName = null): array;
 	public static function fileGetSignedUrl(string $fileId): string;
